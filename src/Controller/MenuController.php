3ca3c6122bdfd43f2d851f820a337773c3dbc290
Tchoubi11 <tchoubayefranck@gmail.com>
2026-02-25 22:47:49 +0100

    Mise à jour de MenuController.php et menu/index.html.twig pour gérer les filtres

--- src/Controller/MenuController.php
+++ src/Controller/MenuController.php
@@ -12,43 +12,51 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class MenuController extends AbstractController
 {
-    #[Route('/menus', name: 'app_menus')]
-    public function index(MenuRepository $menuRepository): Response
-    {
-        return $this->render('menu/index.html.twig', [
-            'menus' => $menuRepository->findAll()
-        ]);
+   #[Route('/menus', name: 'app_menus')]
+public function index(MenuRepository $menuRepository): Response
+{
+    return $this->render('menu/index.html.twig', [
+        'menus' => $menuRepository->findAll()
+    ]);
+}
+
+#[Route('/menus/filter', name: 'app_menus_filter')]
+public function filter(Request $request, MenuRepository $repo): JsonResponse
+{
+    $maxPrice = $request->query->get('maxPrice');
+
+    $qb = $repo->createQueryBuilder('m');
+
+    if ($maxPrice !== null && $maxPrice !== '') {
+        $qb->where('m.prix <= :maxPrice')
+           ->setParameter('maxPrice', (float) $maxPrice);
     }
 
-    #[Route('/menus/{id}', name: 'app_menu_show')]
-    public function show(Menu $menu): Response
-    {
-        return $this->render('menu/show.html.twig', [
-            'menu' => $menu
-        ]);
+    $menus = $qb->getQuery()->getResult();
+
+    $data = [];
+
+    foreach ($menus as $menu) {
+        $firstImage = $menu->getImages()->first();
+
+        $data[] = [
+            'id' => $menu->getId(),
+            'titre' => $menu->getTitre(),
+            'description' => $menu->getDescription(),
+            'prix' => $menu->getPrix(),
+            'nombrePersonnesMinimum' => $menu->getNombrePersonnesMinimum(),
+            'image' => $firstImage ? $firstImage->getUrl() : null,
+        ];
     }
 
-    #[Route('/menus/filter', name: 'app_menus_filter')]
-    public function filter(Request $request, MenuRepository $repo): JsonResponse
-    {
-        $maxPrice = $request->query->get('maxPrice');
-        $theme = $request->query->get('theme');
-        $regime = $request->query->get('regime');
+    return $this->json($data);
+}
 
-        $menus = $repo->filterMenus($maxPrice, $theme, $regime);
-
-        $data = [];
-
-        foreach ($menus as $menu) {
-            $data[] = [
-                'id' => $menu->getId(),
-                'titre' => $menu->getTitre(),
-                'description' => $menu->getDescription(),
-                'prix' => $menu->getPrix(),
-                'nombrePersonnesMinimum' => $menu->getNombrePersonnesMinimum(),
-            ];
-        }
-
-        return $this->json($data);
-    }
+#[Route('/menus/{id<\d+>}', name: 'app_menu_show')]
+public function show(Menu $menu): Response
+{
+    return $this->render('menu/show.html.twig', [
+        'menu' => $menu
+    ]);
+}
 }
