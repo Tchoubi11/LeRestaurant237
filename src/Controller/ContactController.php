0e13d1f1d9234c4f3969356939bb52a8f672e46e
Tchoubi11 <tchoubayefranck@gmail.com>
2026-03-23 20:34:48 +0100

    Création du formulaire de contact et modification de contactcontroller.php et contact/index.html.twig pour la gestion des eamils après les commandes

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -5,12 +5,40 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Form\ContactType;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 
-final class ContactController extends AbstractController
+class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-public function index(): Response
-{
-    return $this->render('contact/index.html.twig');
-}
+    public function index(Request $request, MailerInterface $mailer): Response
+    {
+        $form = $this->createForm(ContactType::class);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $data = $form->getData();
+
+            $email = (new Email())
+                ->from('contact@vitetgourmand.com') 
+                ->replyTo($data['email'])
+                ->to('contact@vitetgourmand.com')
+                ->subject($data['titre'])
+                ->text($data['description']);
+
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Message envoyé avec succès !');
+
+            return $this->redirectToRoute('app_contact');
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'form' => $form
+        ]);
+    }
 }
